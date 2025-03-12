c0013d06b28c16ab4c5d30c04735db5736849bfb
liteeqation <shourya.0706@gmail.com>
2025-03-12 23:27:06 +0530

    Number of videos changed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
   const [loading, setLoading] = useState(true);
   const [loadedVideos, setLoadedVideos] = useState(0);
 
-  const totalVideos = 5;
+  const totalVideos = 4;
   const nextVdRef = useRef(null);
 
   const handleVideoLoad = () => {
